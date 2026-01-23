2f6c5ab4f2ae27f9308ed8ccb2cee0dc3c67d407
Calonder <androsenkoaleksandr6@gmail.com>
2026-01-23 18:12:14 +0300

    feat: добавлены поля профиля (возраст, направление, курс, навыки),миграция, обновил dto-шки, проверил api

---
+++ src/CareerPoint.Infrastructure/Migrations/20260123133710_AddUserFields.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareerPoint.Infrastructure.Migrations
+{
+    public partial class AddUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Course",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Direction",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Skills",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Direction",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Skills",
+                table: "Users");
+        }
+    }
+}
