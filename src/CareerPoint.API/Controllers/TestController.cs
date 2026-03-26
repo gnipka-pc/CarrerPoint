3da0dd1cc095fd6a243f2eee81bc96c52eec34d7
gnipka <gnipka29@gmail.com>
2026-03-26 20:34:47 +0300

    feat: добавлен тестовый контроллер

---
+++ src/CareerPoint.API/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CareerPoint.Web.Controllers;
+
+[Route("api/tests")]
+[ApiController]
+public class TestController: ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateTest()
+    {
+        return Ok();
+    }
+}
