8af64a944f2a40c551f14d01b7bb9e8e9dcd3523
gnipka <gnipka29@gmail.com>
2026-03-26 21:50:53 +0300

    feat: добавили метод удаления

--- src/CareerPoint.API/Controllers/TestController.cs
+++ src/CareerPoint.API/Controllers/TestController.cs
@@ -9,6 +9,13 @@ public class TestController: ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateTest()
     {
+        var b = "test2";
+        return Ok(b);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteTest()
+    {
         return Ok();
     }
 }
