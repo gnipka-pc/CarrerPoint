164cf55da9d1e5096db7b48e3377e1cef097d6e7
Mikhail <michaelluntik.228@gmail.com>
2026-04-03 21:50:00 +0300

    2

--- src/CareerPoint.Infrastructure/Migrations/20260322110428_AddEventFavorites.cs
+++ src/CareerPoint.Infrastructure/Migrations/20260322110428_AddEventFavorites.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -17,7 +18,7 @@ namespace CareerPoint.Infrastructure.Migrations
                 {
                     UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
                     EventId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
-                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP(6)")
                 },
                 constraints: table =>
                 {
